9e60b4d7192663f2bd6773a36f7af828168de664
Edleron <Edleron@hotmail.com>
2024-01-05 17:27:26 +0300

    fix : main scene

--- src/scripts/MainScene.js
+++ src/scripts/MainScene.js
@@ -1,6 +1,10 @@
 import * as PIXI from "pixi.js";
 import { Globals } from "./Globals";
 import { Background } from "./Background";
+import { Platforms } from "./Platforms";
+import { Hero } from "./Hero";
+import { LabelScore } from "./LabelScore";
+import { FinalScene } from "./FinalScene";
 
 export class MainScene {
     constructor() {
@@ -10,6 +14,17 @@ export class MainScene {
         //     volume: 0.2
         // });
         this.createBackground();
+        this.createPlatforms();
+        this.createHero();
+        this.createUI();
+    }
+
+    createUI() {
+        this.labelScore = new LabelScore();
+        this.container.addChild(this.labelScore);
+        this.hero.sprite.on("score", () => {
+            this.labelScore.renderScore(this.hero.score);
+        });
     }
 
     createBackground() {
@@ -17,7 +32,28 @@ export class MainScene {
         this.container.addChild(this.bg.container);
     }
 
+    createPlatforms() {
+        this.platfroms = new Platforms();
+        this.container.addChild(this.platfroms.container);
+    }
+
+
+    createHero() {
+        this.hero = new Hero();
+        this.container.addChild(this.hero.sprite);
+        this.container.interactive = true;
+        this.container.on("pointerdown", () => {
+            this.hero.startJump();
+        });
+        this.hero.sprite.once("die", () => {
+            Globals.scene.start(new FinalScene(this.hero.score));
+        });
+    }
+
     update(dt) {
         this.bg.update(dt);
+        this.platfroms.checkCollision(this.hero);
+        this.platfroms.update(dt);
+        this.hero.update(dt);
     }
 }
